6793d0f533fa3245baa045126f59d301c61c426e
darren <darren.kooyz@gmail.com>
2024-07-01 14:23:26 +0800

    fix: config gteway

--- config/payment-gateway.php
+++ config/payment-gateway.php
@@ -10,7 +10,6 @@ return [
     ],
     'testLive' => [
         'paymentUrl' => '',
-        // 'apiKey' => 'b2680272-958b-41a1-bd97-d52ad9fcbd41',
         'appId' => 'metafinx',
         'endPoint' => 'https://apilist.tronscanapi.com/api/contracts/smart-contract-triggers-batch?fields=hash,method',
         'merchantId' => '1',
@@ -18,7 +17,6 @@ return [
         'callBackUrl' => 'testing_payment/depositCallback',
     ],
     'live' => [
-        'apiKey' => 'b2680272-958b-41a1-bd97-d52ad9fcbd41',
         'appId' => 'metafinx',
     ],
 ];
