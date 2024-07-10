d3dd130e1c3fab02cf21cf15305aba781c8b403b
darren <darren.kooyz@gmail.com>
2024-07-10 14:12:43 +0800

    fix: test cronjob

--- app/Console/Commands/CheckDepositStatus.php
+++ app/Console/Commands/CheckDepositStatus.php
@@ -61,7 +61,7 @@ class CheckDepositStatus extends Command
                             
                         } else {
                             Log::debug('Transaction ID does not exist', $transaction['transaction_id']);
-                            Log::debug('pending row', $pending);
+                            // Log::debug('pending row', $pending);
 
                             $txnAmount = $transaction['value'] / 1000000;
                             $timestamp = $transaction['block_timestamp'] / 1000;
@@ -83,29 +83,29 @@ class CheckDepositStatus extends Command
                             $vCode = md5($pending->transaction_number . $selectedPayout['appId'] . $selectedPayout['merchantId']);
                             $token = Str::random(32);
 
-                            $params = [
-                                'merchant_id' => $pending->merchant_id,
-                                'client_id' => $pending->client_id,
-                                'transaction_type' => $pending->transaction_type,
-                                'from_wallet' => $pending->from_wallet,
-                                'to_wallet' => $pending->to_wallet,
-                                'txID' => $pending->txID,
-                                'block_time' => $pending->block_time,
-                                'transfer_amount' => $pending->txn_amount,
-                                'transaction_number' => $pending->transaction_number,
-                                'amount' => $pending->amount,
-                                'status' => $pending->status,
-                                'payment_method' => $pending->payment_method,
-                                'created_at' => $pending->created_at,
-                                'description' => $pending->description,
-                                'vCode' => $vCode,
-                                'token' => $token,
-                            ];
+                            // $params = [
+                            //     'merchant_id' => $pending->merchant_id,
+                            //     'client_id' => $pending->client_id,
+                            //     'transaction_type' => $pending->transaction_type,
+                            //     'from_wallet' => $pending->from_wallet,
+                            //     'to_wallet' => $pending->to_wallet,
+                            //     'txID' => $pending->txID,
+                            //     'block_time' => $pending->block_time,
+                            //     'transfer_amount' => $pending->txn_amount,
+                            //     'transaction_number' => $pending->transaction_number,
+                            //     'amount' => $pending->amount,
+                            //     'status' => $pending->status,
+                            //     'payment_method' => $pending->payment_method,
+                            //     'created_at' => $pending->created_at,
+                            //     'description' => $pending->description,
+                            //     'vCode' => $vCode,
+                            //     'token' => $token,
+                            // ];
 
-                            $callBackUrl = $selectedPayout['paymentUrl'] . $selectedPayout['callBackUrl'];
-                            $response = Http::post($callBackUrl, $params);
+                            // $callBackUrl = $selectedPayout['paymentUrl'] . $selectedPayout['callBackUrl'];
+                            // $response = Http::post($callBackUrl, $params);
 
-                            Log::debug('$pending', $pending);
+                            // Log::debug('$pending', $pending);
                         }
                     }
                 } else {
