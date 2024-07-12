8b1068e2936ce583b540a939141d398504b0b1a8
darren <darren.kooyz@gmail.com>
2024-07-12 14:44:28 +0800

    fix: nile

--- resources/js/Pages/Auto/ValidPayment.jsx
+++ resources/js/Pages/Auto/ValidPayment.jsx
@@ -55,8 +55,8 @@ export default function Payment({ merchant, transaction, expirationTime, amount,
     useEffect(() => {
         const fetchBlock = async () => {
             try {
-                const response = await fetch('https://api.trongrid.io/walletsolidity/getnowblock');
-                // const response = await fetch('https://nile.trongrid.io/walletsolidity/getnowblock');
+                // const response = await fetch('https://api.trongrid.io/walletsolidity/getnowblock');
+                const response = await fetch('https://nile.trongrid.io/walletsolidity/getnowblock');
                 const result = await response.json();
                 const timestamp = result.block_header.raw_data.timestamp;
                 setBlockTimestamp(timestamp);
@@ -73,8 +73,8 @@ export default function Payment({ merchant, transaction, expirationTime, amount,
         // let pollingInterval;
         const fetchTransactions = async () => {
             try {
-                const url = `https://api.trongrid.io/v1/accounts/${tokenAddress}/transactions/trc20?order_by=block_timestamp,desc&min_timestamp=${blockTimestamp}`;
-                // const url = `https://nile.trongrid.io/v1/accounts/${tokenAddress}/transactions/trc20?order_by=block_timestamp,desc&min_timestamp=${blockTimestamp}`;
+                // const url = `https://api.trongrid.io/v1/accounts/${tokenAddress}/transactions/trc20?order_by=block_timestamp,desc&min_timestamp=${blockTimestamp}`;
+                const url = `https://nile.trongrid.io/v1/accounts/${tokenAddress}/transactions/trc20?order_by=block_timestamp,desc&min_timestamp=${blockTimestamp}`;
                 const response = await fetch(url);
                 const result = await response.json();
                 console.log(result);
